bf63476e3c22ff50755af9fba19374990f157057
Michael <102828476+HappyPanda6453@users.noreply.github.com>
2022-06-14 17:04:24 -0400

    Add files via upload

---
+++ Java-Quiz/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+      title: "A boolean expression has how many values?",
+      multiChoice: ["0", "1", "2", "3"],
+      answer: "2"
+    },
+  
+    {
+      title: "How do we comment in HTML?",
+      multiChoice: ["# -Followed by the comment", "// -Followed by the comment", "<!--Comment-->", "- -Followed by the comment"],
+      answer: "<!--Comment-->"
+    },
+  
+    {
+      title: "All things in Java are known as",
+      multiChoice: ["Things", "Objects", "Items", "Arrays"],
+      answer: "Objects"
+    },
+  
+    {
+      title: "All normal web pages consist of",
+      multiChoice: ["A top and a bottom", "A head and a body", "A body and a framework", "None of the above"],
+      answer: "A head and a body"
+    },
+  
+    {
+      title: "The color #FFFFFF will chow what color?",
+      multiChoice: ["Black", "White", "Red", "No color"],
+      answer: "White"
+    }
+  ];
